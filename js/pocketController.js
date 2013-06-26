6a5833c708e67e636d007c21da95c89b190690fe
Diego Nakamashi <diegonakamashi@gmail.com>
2013-06-26 11:07:50 -0300

    começo da implementação com o angular -- nada funciona ainda

---
+++ js/pocketController.js
@@ -0,0 +1,11 @@
+function PocketController($scope){
+  $scope.getItems = function(){
+    return [
+      {
+        faviconUrl: "url",
+        url: "www.google.com",
+        title: "Google"
+      }
+    ]
+  }  
+}
---
+++ js/readItLaterRepository.js
@@ -0,0 +1,9 @@
+function ReadItLaterRepository(){
+  $scope.items = [
+    {
+      faviconUrl: "a",
+      url: "www.google.com",
+      title: "Google"
+    }
+  ]
+}
